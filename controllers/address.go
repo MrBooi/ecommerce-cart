a3bfe969d579d22c364757041034b42caef698eb
Ayabonga <ayabongabooi2@gmail.com>
2023-09-16 23:29:29 +0200

    wip(controllers): added signup and login implementation

--- controllers/address.go
+++ controllers/address.go
@@ -3,17 +3,25 @@ package controllers
 import "github.com/gin-gonic/gin"
 
 func AddAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 
+	}
 }
 
 func EditHomeAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 
+	}
 }
 
 func EditWorkHomeAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 
+	}
 }
 
 func DeleteAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 
+	}
 }
