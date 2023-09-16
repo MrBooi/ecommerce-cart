89e55ef93d83c301e989115cbef6d0fc9e99bff2
Ayabonga <ayabongabooi2@gmail.com>
2023-09-16 21:48:37 +0200

    feat(controllers): added controller controllers skeleton

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,29 @@
 package controllers
+
+import ()
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {}
+
+func VerifyPassword(userPassword, givenPassword string) (bool, string) {}
+
+func SignUp() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {
+
+}
