f40870142b930c440d3d342189a347b5dabe9bb1
Ayabonga <ayabongabooi2@gmail.com>
2023-09-16 20:19:21 +0200

    feat(routes): setup user routes fix typo

--- routes/routes.go
+++ routes/routes.go
@@ -9,6 +9,6 @@ func userRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/user/signup", controllers.SignUp())
 	incomingRoutes.POST("/user/login", controllers.Login())
 	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
-	incomingRoutes.POST("/users/productview", controllers.SearchProduct())
-	incomingRoutes.POST("/user/search", controllers.SearchProductByQuery())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/user/search", controllers.SearchProductByQuery())
 }
