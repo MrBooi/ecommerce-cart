cf77d6e078e0ff5fbfbbd85eb48a0906d68795cd
Ayabonga <ayabongabooi2@gmail.com>
2023-09-16 21:48:04 +0200

    feat(address): added controller address skeleton

--- controllers/address.go
+++ controllers/address.go
@@ -1,1 +1,19 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddAddress() gin.HandlerFunc {
+
+}
+
+func EditHomeAddress() gin.HandlerFunc {
+
+}
+
+func EditWorkHomeAddress() gin.HandlerFunc {
+
+}
+
+func DeleteAddress() gin.HandlerFunc {
+
+}
