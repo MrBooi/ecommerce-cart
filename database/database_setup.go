04bb81117f41072445bcde32223bd821122b2e09
Ayabonga <ayabongabooi2@gmail.com>
2023-09-16 22:25:35 +0200

    feat(database_setup): added database setup funcs

--- database/database_setup.go
+++ database/database_setup.go
@@ -1,1 +1,37 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func dbSetup() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Println("Failed to connect to mongodb :( ")
+		return nil
+	}
+
+	fmt.Println("mongodb is successfully connected.")
+
+	return client
+
+}
+
+var Client *mongo.Client = dbSetup()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {}
